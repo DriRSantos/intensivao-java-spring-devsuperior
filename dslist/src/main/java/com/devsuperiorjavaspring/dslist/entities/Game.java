c31b21c8abc4f46d89c4e489dbf33bab72db8bee
DriRSantos <drirsantos@outlook.com>
2023-05-12 14:35:17 -0300

    GFame seed

--- dslist/src/main/java/com/devsuperiorjavaspring/dslist/entities/Game.java
+++ dslist/src/main/java/com/devsuperiorjavaspring/dslist/entities/Game.java
@@ -23,15 +23,19 @@ public class Game {
 	private String genre;
 	private String platforms;
 	private Double score;
-	private String imdUrl;
+	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 		
 	}
 	
-	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imdUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
@@ -39,7 +43,7 @@ public class Game {
 		this.genre = genre;
 		this.platforms = platforms;
 		this.score = score;
-		this.imdUrl = imdUrl;
+		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
 	}
@@ -93,11 +97,11 @@ public class Game {
 	}
 
 	public String getImdUrl() {
-		return imdUrl;
+		return imgUrl;
 	}
 
 	public void setImdUrl(String imdUrl) {
-		this.imdUrl = imdUrl;
+		this.imgUrl = imdUrl;
 	}
 
 	public String getShortDescription() {
